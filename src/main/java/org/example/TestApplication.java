d8c9ccb1992bac5e443430f68809fb97eed0a399
junhyeok <jhsh9912@gmail.com>
2024-01-31 14:36:07 +0900

    {test} 프로그램 test

---
+++ src/main/java/org/example/TestApplication.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class TestApplication {
+    //수업목표 오늘의 수업 목표입니다.
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("나는 개발자 이다."); //출력
+
+    }
+}
